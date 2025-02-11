0b49bd1ab00b1343299fbbe94f10134f2a19773f
Shafayat Mahin <shafayatmahin5@gmail.com>
2025-02-11 17:06:13 +0600

    load top foods

--- client/src/pages/Home/components/TopFoods.jsx
+++ client/src/pages/Home/components/TopFoods.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const TopFoods = () => {
+  const [topFoods, setTopFoods] = useState([]);
+
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/top-foods")
+      .then(response => {
+        console.log(response.data);
+        setTopFoods(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+      
+  }, [])
+
   return (
     <div>
-      <h1 className="text-3xl">This is the TopFoods</h1>      
+      <h1 className="text-3xl">This is the TopFoods: {topFoods.length}</h1>
     </div>
   );
 };
--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
+import cors from 'cors';
 import { MongoClient, ServerApiVersion } from 'mongodb';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(express.json());
+app.use(cors());
 
 // const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.hjueq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hjueq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -29,11 +34,24 @@ async function run() {
     const database = client.db("restaurantDB");
     const foodCollection = database.collection("foods");
 
-    // load sample data
+    // loading foods data api
+
+    // load all foods
     app.get("/foods", async (req, res) => {
       const result = await foodCollection.find({}).toArray();
       res.send(result);
     });
+
+    // load top 6 foods
+    app.get("/top-foods", async (req, res) => {
+      const result = await foodCollection.find()
+      .sort({purchase_count: -1})
+      .limit(6)
+      .toArray();
+
+      res.send(result);
+
+    })
 
 
 
