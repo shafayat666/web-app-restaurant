c92d72a3908d85a0830906cec2d5db6c38117709
Shafayat Mahin <shafayatmahin5@gmail.com>
2025-02-11 17:17:17 +0600

    creat view details button

--- client/src/pages/Home/components/TopFoods.jsx
+++ client/src/pages/Home/components/TopFoods.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import FoodCard from "../../shared/FoodCard";
 
 const TopFoods = () => {
   const [topFoods, setTopFoods] = useState([]);
@@ -14,12 +15,19 @@ const TopFoods = () => {
       .catch(error => {
         console.log(error);
       });
-      
+
   }, [])
 
   return (
     <div>
-      <h1 className="text-3xl">This is the TopFoods: {topFoods.length}</h1>
+      <h1 className="text-3xl text-center my-8">This is the TopFoods: {topFoods.length}</h1>
+      <div className="grid grid-cols-3 gap-4">
+        {
+          topFoods.map((food, index) => (
+            <FoodCard key={index} food={food} />
+          ))
+        }
+      </div>
     </div>
   );
 };
---
+++ client/src/pages/shared/FoodCard.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router';
+
+const FoodCard = ({ food }) => {
+  const { _id, name, image, category, quantity, price, addBy, origin, description, purchase_count } = food;
+  return (
+    <div>
+      <div className="p-4">
+        <h1 className="text-xl font-bold">{name}</h1>
+        <p>{description}</p>
+        <p>Price: {price}</p>
+        <p>Purchase Count: {purchase_count}</p>
+        <Link to={`/food/${_id}`} className="btn btn-primary">View Details</Link>
+      </div>
+    </div>
+  );
+};
+
+export default FoodCard;
+
+FoodCard.propTypes = {
+  food: PropTypes.object,
+}
