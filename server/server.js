93680dceee296771ed7cf917ca67abff44b16af6
Shafayat Mahin <shafayatmahin5@gmail.com>
2025-02-11 15:57:49 +0600

    feat: get all foods data

--- server/server.js
+++ server/server.js
@@ -1,7 +1,51 @@
 import express from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.hjueq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hjueq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    // create a database and collection
+    const database = client.db("restaurantDB");
+    const foodCollection = database.collection("foods");
+
+    // load sample data
+    app.get("/foods", async (req, res) => {
+      const result = await foodCollection.find({}).toArray();
+      res.send(result);
+    });
+
+
+
+
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
