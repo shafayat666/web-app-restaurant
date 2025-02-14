8a0396b655e5fac73d3c5e0636e362816d8bb547
Shafayat Mahin <shafayatmahin5@gmail.com>
2025-02-14 17:14:07 +0600

    feat: add search functionality to all-foods

--- client/src/pages/AllFoods/AllFoods.jsx
+++ client/src/pages/AllFoods/AllFoods.jsx
@@ -3,20 +3,43 @@ import FoodCard from "../shared/FoodCard";
 import { useState } from "react";
 
 const AllFoods = () => {
-  const loadFoods = useLoaderData();
-  const [foods, setFoods] = useState(loadFoods);
+  const foods = useLoaderData();
+  const [search, setSearch] = useState("");
+  // console.log(search);
+
+  const filteredFoods = foods.filter(food => {
+    return food.name.toLowerCase().includes(search.toLowerCase());
+  });
 
   return (
     <div>
       <h1 className="text-6xl text-center">All of our Foods</h1>
-      <input type="text" placeholder="Search for food" className="w-full p-4 my-4" />
-      <div className="grid grid-cols-1 gap-4">
+      <input
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+        type="text"
+        placeholder="Search for food"
+        className="w-full p-4 my-4" />
+
+      {
+        filteredFoods.length > 0 ?
+        <div className="grid grid-cols-1 gap-4">
+          {
+            filteredFoods.map((food, index) => (
+              <FoodCard key={index} food={food} />
+            ))
+          }
+        </div>
+        :
+        <h1 className="text-4xl text-center">No food found</h1>
+      }
+      {/* <div className="grid grid-cols-1 gap-4">
         {
           foods.map((food, index) => (
             <FoodCard key={index} food={food} />
           ))
         }
-      </div>
+      </div> */}
     </div>
   );
 };
