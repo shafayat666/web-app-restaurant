b96ccf753cde0e02cc8091b283e35f9bdbc33ec8
Shafayat Mahin <shafayatmahin5@gmail.com>
2025-02-15 19:36:43 +0600

    feat: theme toggle

--- client/src/pages/shared/NavBar.jsx
+++ client/src/pages/shared/NavBar.jsx
@@ -1,10 +1,23 @@
 import { Link, useNavigate } from "react-router";
 import useAuth from "../../hooks/useAuth";
 import Swal from "sweetalert2";
+import { useEffect, useState } from "react";
 
 const NavBar = () => {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
   const { user, logout } = useAuth();
   const navigate = useNavigate();
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+    localStorage.setItem("theme", newTheme);
+    document.documentElement.setAttribute("data-theme", newTheme);
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
 
   const links = <>
     <Link className="mr-4" to="/">Home</Link>
@@ -61,6 +74,42 @@ const NavBar = () => {
         </ul>
       </div>
       <div className="navbar-end">
+        {/* theme controller */}
+        <label className="flex cursor-pointer gap-2 mr-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round">
+            <circle cx="12" cy="12" r="5" />
+            <path
+              d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+          </svg>
+          <input
+            checked={theme === "dark"}
+            onChange={toggleTheme}
+            type="checkbox"
+            value="synthwave"
+            className="toggle theme-controller" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round">
+            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+          </svg>
+        </label>
+
         {
           user ?
             <>
